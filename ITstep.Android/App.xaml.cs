b7ff63a32e6fec7a23c7a3f84e26219b2ff3c7be
Denis <ponomarevd61@gmail.com>
2024-04-20 13:12:06 +0300

    start

---
+++ ITstep.Android/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ITstep.Android
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
