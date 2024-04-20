43f8460006e387264ccb60973e4b0e9800c697a3
Denis <ponomarevd61@gmail.com>
2024-04-20 14:18:18 +0300

    add user

---
+++ ITstep.Desktop/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ITstep.Desktop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ITstep.Domen/StepDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ITstep.Domen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITstep.Domen;
+
+public class StepDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Step;Trusted_Connection=True");
+    }
+}
